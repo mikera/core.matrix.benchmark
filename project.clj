4a950807ace93bd8f84f882c055e0fa915c20ed7
mikera <mike.r.anderson.13@gmail.com>
2013-10-28 18:26:17 +0800

    add benchmark namespace and basic dependencies

--- project.clj
+++ project.clj
@@ -3,7 +3,9 @@
   :url "https://github.com/mikera/core.matrix.benchmark"
   :dev-dependencies [[lein-expectations "0.0.8"]
                      [expectations "1.4.41"]]
-  :dependencies [[org.clojure/clojure "1.4.0"]
-                 [net.mikera/core.matrix "0.13.0"]]
+  :dependencies [[org.clojure/clojure "1.5.1"]
+                 [net.mikera/core.matrix "0.13.0"]
+                 [net.mikera/vectorz-clj "0.15.0"]
+                 [clatrix "0.3.0"]]
   :profiles {:dev {:dependencies [[criterium/criterium "0.4.1"]
                                   [expectations "1.4.41"]]}})
---
+++ src/clojure/core/matrix/benchmark.clj
@@ -0,0 +1,2 @@
+(ns clojure.core.matrix.benchmark
+  (:use clojure.core.matrix))
