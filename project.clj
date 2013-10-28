fc7a9fb8b93d0e58a9c2cac58a92e9094ce398a3
mikera <mike.r.anderson.13@gmail.com>
2013-10-28 18:34:51 +0800

    use snapshot clatrix

--- project.clj
+++ project.clj
@@ -6,6 +6,6 @@
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [net.mikera/core.matrix "0.13.0"]
                  [net.mikera/vectorz-clj "0.15.0"]
-                 [clatrix "0.3.0"]]
+                 [clatrix "0.4.0-SNAPSHOT"]]
   :profiles {:dev {:dependencies [[criterium/criterium "0.4.1"]
                                   [expectations "1.4.41"]]}})
