772b846628d5a702e52d7008b5a36587258177e4
mikera <mike.r.anderson.13@gmail.com>
2013-11-09 13:31:03 +0800

    update deps

--- project.clj
+++ project.clj
@@ -4,9 +4,9 @@
   :dev-dependencies [[lein-expectations "0.0.8"]
                      [expectations "1.4.41"]]
   :dependencies [[org.clojure/clojure "1.5.1"]
-                 [net.mikera/core.matrix "0.13.0"]
-                 [net.mikera/core.matrix.stats "0.2.0"]
-                 [net.mikera/vectorz-clj "0.15.0"]
+                 [net.mikera/core.matrix "0.15.0"]
+                 [net.mikera/core.matrix.stats "0.3.0"]
+                 [net.mikera/vectorz-clj "0.16.1-SNAPSHOT"]
                  [clatrix "0.4.0-SNAPSHOT"]]
   :profiles {:dev {:dependencies [[criterium/criterium "0.4.1"]
                                   [expectations "1.4.41"]]}})
